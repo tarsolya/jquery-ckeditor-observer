768825f31001eabc62c61142687e5533cb7ce289
Tarsoly András <tarsolya@gmail.com>
2009-12-18 06:17:25 +0100

    Initial commit.

---
+++ jquery.ckeditor.observer.js
@@ -0,0 +1,45 @@
+/**
+ * jQuery Observer for CKEDITOR
+ *
+ * @author Tarsoly András (tarsoly[dot]andras[at]xpnindustries[dot]com)
+ *
+ * Licensed under the MIT License
+ *
+ * Usage: 
+ * $("form").observe(10, function() {
+ *   console.log("It's dirty!")
+ * });
+ *
+ * Checks the form each 10 seconds. If the form has changes since last time including CKEDITOR instances,
+ * the value of the original textarea elements gets updated from the respective CKEDITOR instance,
+ * then the callback gets called.
+ * 
+ * Great for implementing autosave feature via Ajax in combination with jQuery Forms.
+ * 
+ * TODO:
+ *   - make specific CKEDITOR instances targetable
+ */
+
+$.fn.observe = function(seconds, callback){
+	return this.each(function(){
+		var form = this, dirty = false;
+
+		$(form.elements).change(function(){
+    	dirty = true;
+		});
+
+		setInterval(function(){
+			if(CKEDITOR) {
+				for(var instanceName in CKEDITOR.instances) {
+					if(CKEDITOR.instances[instanceName].checkDirty()) {
+						$('textarea#' + instanceName).val(CKEDITOR.instances[instanceName].getData());
+						dirty = true;
+						CKEDITOR.instances[instanceName].resetDirty();
+					};
+				}
+			}
+    	if (dirty) callback.call(form);
+      dirty = false;
+		}, seconds * 1000);
+	});
+};
